4cabc4f3ee812f20d560914bf92ddbd0c9784796
imicadio <mrjesio@gmail.com>
2018-04-20 20:50:10 +0200

    ID i CODE wyciągnięte

--- src/app/resetPassword/resetPassword.component.ts
+++ src/app/resetPassword/resetPassword.component.ts
@@ -12,11 +12,21 @@ export class ResetPasswordComponent {
 
   model = new Password('', '');
   submitted = false;
+  pathString: any;      // Ścieżka z linku
+  id: any;              // ID
+  kod: any;             // code
 
   constructor(private location: Location) {
-    var pathString = location.path();
-      console.log('appComponent: pathString...');
-      console.log(pathString);
+      this.pathString = location.path();                    // Arecki inicjuje ścieżkę z neta
+      console.log('appComponent: pathString...');           // Console.log XDDD Arecki robi na oślep ale punkt za wyciągnięcie ścieżki! 
+      var papa = this.pathString;                           // Ścieżke do zmiennej LOKALNEJ przypisuje (łatwiej się pracuje podczas modyfikacji stringu)
+      var liczba1 = papa.indexOf('=');                      // Ścieżka jest charakterystyczna, posiada dwa znaki '=' dlatego szukam pozycje pierwszego z nich
+      var liczba2 = papa.indexOf('&');                      // Szukam pozycji '&' ponieważ na tym kończy mi się id użytwkownika
+      var _liczba1 = papa.indexOf('=', liczba1 + 1);        // Szukam kolejnej pozycji znaku '='
+      this.id = papa.substring(liczba1 + 1, liczba2);       // wyciągam id użytkownika przycinając ścieżkę
+      this.kod = papa.substring(_liczba1 + 1, );            // wyciagam kod użytkownika
+      console.log('ID użytkownika: ' + this.id);      
+      console.log('Kod użytkownika: ' + this.kod);
    }
 
   onSubmit() {
