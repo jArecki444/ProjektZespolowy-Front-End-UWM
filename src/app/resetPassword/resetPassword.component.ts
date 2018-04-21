37640d0f5b762e577c9a9c7b8106b78b71bb27ad
jArecki444 <arkadiusz.cichurski@gmail.com>
2018-04-21 21:48:25 +0200

    dodanie metody do wyłuskania poprawnego wyłuskania url

--- src/app/resetPassword/resetPassword.component.ts
+++ src/app/resetPassword/resetPassword.component.ts
@@ -19,23 +19,31 @@ export class ResetPasswordComponent {
   constructor(private location: Location) {
       this.pathString = location.path();                    // Arecki inicjuje ścieżkę z neta
       console.log('appComponent: pathString...');           // Console.log XDDD Arecki robi na oślep ale punkt za wyciągnięcie ścieżki! 
-      var papa = this.pathString;                           // Ścieżke do zmiennej LOKALNEJ przypisuje (łatwiej się pracuje podczas modyfikacji stringu)
+      var test = this.pathString;
+      var papa = this.standardEncoding(test);                           // Ścieżke do zmiennej LOKALNEJ przypisuje (łatwiej się pracuje podczas modyfikacji stringu)
       var liczba1 = papa.indexOf('=');                      // Ścieżka jest charakterystyczna, posiada dwa znaki '=' dlatego szukam pozycje pierwszego z nich
       var liczba2 = papa.indexOf('&');                      // Szukam pozycji '&' ponieważ na tym kończy mi się id użytwkownika
       var _liczba1 = papa.indexOf('=', liczba1 + 1);        // Szukam kolejnej pozycji znaku '='
-      this.id = papa.substring(liczba1 + 1, liczba2);       // wyciągam id użytkownika przycinając ścieżkę
+      this.id = test.substring(liczba1 + 1, liczba2);       // wyciągam id użytkownika przycinając ścieżkę
       this.kod = papa.substring(_liczba1 + 1, );            // wyciagam kod użytkownika
-      console.log('ID użytkownika: ' + this.id);      
+      console.log('ID użytkownika: ' + this.id);
       console.log('Kod użytkownika: ' + this.kod);
       this.model = new Password(this.id, this.kod, '', '');
    }
 
-   
-
-  SendNewPassword() {    
+  SendNewPassword() {
     this.submitted = true;
     console.log(this.model);
-
   }
 
+  standardEncoding(v: string): string {
+    return encodeURIComponent(v)
+    .replace(/%2F/gi, '')
+    .replace(/%3D/gi, '=')
+    .replace(/%2520/gi, '+')
+    .replace(/%252F%252F/gi, '//')
+    .replace(/%252F/gi, '/')
+    .replace(/%253D%253D/gi, '==') // zamieniamy ciąg blablaba na == i dopiero po tym zapisujemy sciezke
+    .replace(/%3F/gi, '?');
+  }
 }
